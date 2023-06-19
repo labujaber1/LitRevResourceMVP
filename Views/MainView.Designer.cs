f98c2acd8957500dd1a26fdc76aff29ff805b4f0
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2023-06-19 14:51:27 +0100

    Connection wizard made invisible

--- Views/MainView.Designer.cs
+++ Views/MainView.Designer.cs
@@ -29,125 +29,134 @@ namespace LitRevResourceMVP.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.Btn_MainModule = new System.Windows.Forms.Button();
-            this.Btn_MainAssignments = new System.Windows.Forms.Button();
-            this.Btn_MainResource = new System.Windows.Forms.Button();
-            this.Lbl_MainMenu = new System.Windows.Forms.Label();
-            this.Btn_ExitSystem = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.Btn_ConnWizard = new System.Windows.Forms.Button();
-            this.Btn_DBConnSetup = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
+            Btn_MainModule = new System.Windows.Forms.Button();
+            Btn_MainAssignments = new System.Windows.Forms.Button();
+            Btn_MainResource = new System.Windows.Forms.Button();
+            Lbl_MainMenu = new System.Windows.Forms.Label();
+            Btn_ExitSystem = new System.Windows.Forms.Button();
+            panel1 = new System.Windows.Forms.Panel();
+            Btn_ConnWizard = new System.Windows.Forms.Button();
+            Btn_DBConnSetup = new System.Windows.Forms.Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // Btn_MainModule
             // 
-            this.Btn_MainModule.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_MainModule.BackColor = System.Drawing.SystemColors.Menu;
-            this.Btn_MainModule.Location = new System.Drawing.Point(19, 326);
-            this.Btn_MainModule.Name = "Btn_MainModule";
-            this.Btn_MainModule.Size = new System.Drawing.Size(182, 63);
-            this.Btn_MainModule.TabIndex = 2;
-            this.Btn_MainModule.Text = "Module";
-            this.Btn_MainModule.UseVisualStyleBackColor = false;
+            Btn_MainModule.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_MainModule.BackColor = System.Drawing.SystemColors.Menu;
+            Btn_MainModule.Location = new System.Drawing.Point(15, 261);
+            Btn_MainModule.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_MainModule.Name = "Btn_MainModule";
+            Btn_MainModule.Size = new System.Drawing.Size(146, 50);
+            Btn_MainModule.TabIndex = 2;
+            Btn_MainModule.Text = "Module";
+            Btn_MainModule.UseVisualStyleBackColor = false;
             // 
             // Btn_MainAssignments
             // 
-            this.Btn_MainAssignments.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_MainAssignments.BackColor = System.Drawing.SystemColors.Menu;
-            this.Btn_MainAssignments.Location = new System.Drawing.Point(19, 238);
-            this.Btn_MainAssignments.Name = "Btn_MainAssignments";
-            this.Btn_MainAssignments.Size = new System.Drawing.Size(182, 63);
-            this.Btn_MainAssignments.TabIndex = 1;
-            this.Btn_MainAssignments.Text = "Assignment";
-            this.Btn_MainAssignments.UseVisualStyleBackColor = false;
+            Btn_MainAssignments.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_MainAssignments.BackColor = System.Drawing.SystemColors.Menu;
+            Btn_MainAssignments.Location = new System.Drawing.Point(15, 190);
+            Btn_MainAssignments.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_MainAssignments.Name = "Btn_MainAssignments";
+            Btn_MainAssignments.Size = new System.Drawing.Size(146, 50);
+            Btn_MainAssignments.TabIndex = 1;
+            Btn_MainAssignments.Text = "Assignment";
+            Btn_MainAssignments.UseVisualStyleBackColor = false;
             // 
             // Btn_MainResource
             // 
-            this.Btn_MainResource.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_MainResource.BackColor = System.Drawing.SystemColors.Menu;
-            this.Btn_MainResource.Location = new System.Drawing.Point(19, 156);
-            this.Btn_MainResource.Name = "Btn_MainResource";
-            this.Btn_MainResource.Size = new System.Drawing.Size(182, 63);
-            this.Btn_MainResource.TabIndex = 0;
-            this.Btn_MainResource.Text = "Resources";
-            this.Btn_MainResource.UseVisualStyleBackColor = false;
+            Btn_MainResource.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_MainResource.BackColor = System.Drawing.SystemColors.Menu;
+            Btn_MainResource.Location = new System.Drawing.Point(15, 125);
+            Btn_MainResource.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_MainResource.Name = "Btn_MainResource";
+            Btn_MainResource.Size = new System.Drawing.Size(146, 50);
+            Btn_MainResource.TabIndex = 0;
+            Btn_MainResource.Text = "Resources";
+            Btn_MainResource.UseVisualStyleBackColor = false;
             // 
             // Lbl_MainMenu
             // 
-            this.Lbl_MainMenu.AutoSize = true;
-            this.Lbl_MainMenu.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Lbl_MainMenu.Location = new System.Drawing.Point(12, 38);
-            this.Lbl_MainMenu.Name = "Lbl_MainMenu";
-            this.Lbl_MainMenu.Size = new System.Drawing.Size(200, 48);
-            this.Lbl_MainMenu.TabIndex = 1;
-            this.Lbl_MainMenu.Text = "Main Menu";
+            Lbl_MainMenu.AutoSize = true;
+            Lbl_MainMenu.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            Lbl_MainMenu.Location = new System.Drawing.Point(10, 30);
+            Lbl_MainMenu.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            Lbl_MainMenu.Name = "Lbl_MainMenu";
+            Lbl_MainMenu.Size = new System.Drawing.Size(169, 41);
+            Lbl_MainMenu.TabIndex = 1;
+            Lbl_MainMenu.Text = "Main Menu";
             // 
             // Btn_ExitSystem
             // 
-            this.Btn_ExitSystem.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_ExitSystem.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.Btn_ExitSystem.Location = new System.Drawing.Point(36, 636);
-            this.Btn_ExitSystem.Name = "Btn_ExitSystem";
-            this.Btn_ExitSystem.Size = new System.Drawing.Size(148, 63);
-            this.Btn_ExitSystem.TabIndex = 2;
-            this.Btn_ExitSystem.Text = "Exit system";
-            this.Btn_ExitSystem.UseVisualStyleBackColor = false;
-            this.Btn_ExitSystem.Click += new System.EventHandler(this.Btn_ExitSystem_Click);
+            Btn_ExitSystem.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_ExitSystem.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            Btn_ExitSystem.Location = new System.Drawing.Point(29, 509);
+            Btn_ExitSystem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_ExitSystem.Name = "Btn_ExitSystem";
+            Btn_ExitSystem.Size = new System.Drawing.Size(118, 50);
+            Btn_ExitSystem.TabIndex = 2;
+            Btn_ExitSystem.Text = "Exit system";
+            Btn_ExitSystem.UseVisualStyleBackColor = false;
+            Btn_ExitSystem.Click += Btn_ExitSystem_Click;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panel1.Controls.Add(this.Btn_ConnWizard);
-            this.panel1.Controls.Add(this.Btn_DBConnSetup);
-            this.panel1.Controls.Add(this.Btn_ExitSystem);
-            this.panel1.Controls.Add(this.Lbl_MainMenu);
-            this.panel1.Controls.Add(this.Btn_MainModule);
-            this.panel1.Controls.Add(this.Btn_MainAssignments);
-            this.panel1.Controls.Add(this.Btn_MainResource);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(217, 759);
-            this.panel1.TabIndex = 3;
+            panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            panel1.Controls.Add(Btn_ConnWizard);
+            panel1.Controls.Add(Btn_DBConnSetup);
+            panel1.Controls.Add(Btn_ExitSystem);
+            panel1.Controls.Add(Lbl_MainMenu);
+            panel1.Controls.Add(Btn_MainModule);
+            panel1.Controls.Add(Btn_MainAssignments);
+            panel1.Controls.Add(Btn_MainResource);
+            panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(174, 607);
+            panel1.TabIndex = 3;
             // 
             // Btn_ConnWizard
             // 
-            this.Btn_ConnWizard.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_ConnWizard.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Btn_ConnWizard.Location = new System.Drawing.Point(18, 455);
-            this.Btn_ConnWizard.Name = "Btn_ConnWizard";
-            this.Btn_ConnWizard.Size = new System.Drawing.Size(183, 55);
-            this.Btn_ConnWizard.TabIndex = 4;
-            this.Btn_ConnWizard.Text = "Connection wizard";
-            this.Btn_ConnWizard.UseVisualStyleBackColor = false;
+            Btn_ConnWizard.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_ConnWizard.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            Btn_ConnWizard.Location = new System.Drawing.Point(14, 364);
+            Btn_ConnWizard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_ConnWizard.Name = "Btn_ConnWizard";
+            Btn_ConnWizard.Size = new System.Drawing.Size(146, 44);
+            Btn_ConnWizard.TabIndex = 4;
+            Btn_ConnWizard.Text = "Connection wizard";
+            Btn_ConnWizard.UseVisualStyleBackColor = false;
+            Btn_ConnWizard.Visible = false;
             // 
             // Btn_DBConnSetup
             // 
-            this.Btn_DBConnSetup.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Btn_DBConnSetup.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Btn_DBConnSetup.Location = new System.Drawing.Point(19, 516);
-            this.Btn_DBConnSetup.Name = "Btn_DBConnSetup";
-            this.Btn_DBConnSetup.Size = new System.Drawing.Size(182, 66);
-            this.Btn_DBConnSetup.TabIndex = 3;
-            this.Btn_DBConnSetup.Text = "DB connection setup";
-            this.Btn_DBConnSetup.UseVisualStyleBackColor = false;
+            Btn_DBConnSetup.Anchor = System.Windows.Forms.AnchorStyles.None;
+            Btn_DBConnSetup.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            Btn_DBConnSetup.Location = new System.Drawing.Point(15, 413);
+            Btn_DBConnSetup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_DBConnSetup.Name = "Btn_DBConnSetup";
+            Btn_DBConnSetup.Size = new System.Drawing.Size(146, 53);
+            Btn_DBConnSetup.TabIndex = 3;
+            Btn_DBConnSetup.Text = "DB connection setup";
+            Btn_DBConnSetup.UseVisualStyleBackColor = false;
             // 
             // MainView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1121, 759);
-            this.Controls.Add(this.panel1);
-            this.IsMdiContainer = true;
-            this.Name = "MainView";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainView";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new System.Drawing.Size(897, 607);
+            Controls.Add(panel1);
+            IsMdiContainer = true;
+            Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Name = "MainView";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "MainView";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
